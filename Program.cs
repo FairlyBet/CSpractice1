989072f4a017722c2f2084abd112b380478a1200
Александр Загаревский <fairlybet@gmail.com>
2020-02-28 17:16:27 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSpractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1, then number 2");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Multiplycation of number 1 and number 2 is {Multiply(number1, number2)}");
+        }
+
+        static double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
